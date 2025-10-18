433a5691043eb74295ec89271c1f785379d4ade0
Natnael taye <natitaye316@gmail.com>
2025-10-18 23:16:07 +0300

    feat: add HowItWorksPage component to Home for application process overview

--- kelal/app/(client side)/page.tsx
+++ kelal/app/(client side)/page.tsx
@@ -1,9 +1,10 @@
 import Hero from "./Hero";
-
+import HowItWorksPage from "./HowItWorks";
 export default function Home() {
     return (
         <>
             <Hero />
+            <HowItWorksPage />
         </>
     );
 }
