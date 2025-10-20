95784991b5e47e6527a3f20a99ef9143d34b2ccc
Natnael taye <natitaye316@gmail.com>
2025-10-20 22:37:00 +0300

    feat: add Key Features, Banner, and FAQ components to Home page for enhanced content display

--- kelal/app/(client side)/page.tsx
+++ kelal/app/(client side)/page.tsx
@@ -1,10 +1,17 @@
 import Hero from "./Hero";
 import HowItWorksPage from "./HowItWorks";
+import KeyFeatures from "./KeyFeatures";
+import Banner from "./Banner";
+import FAQ from "./FAQ";
+
 export default function Home() {
     return (
         <>
             <Hero />
             <HowItWorksPage />
+            <KeyFeatures />
+            <Banner />
+            <FAQ/>
         </>
     );
 }
