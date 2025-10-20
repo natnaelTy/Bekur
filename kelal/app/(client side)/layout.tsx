1410d05241ee92d7274a7bd3fa650772e62c5686
Natnael taye <natitaye316@gmail.com>
2025-10-20 22:36:54 +0300

    feat: add ThemeProvider and Footer to ClientLayout for improved theming and layout structure

--- kelal/app/(client side)/layout.tsx
+++ kelal/app/(client side)/layout.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-
+import { ThemeProvider } from "next-themes";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
 
 function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
       <main>{children}</main>
+      <Footer />
     </>
   );
 }
