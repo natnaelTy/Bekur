85c02e506d906f5cd00ceba56d05263cf03fe6e9
Natnael taye <natitaye316@gmail.com>
2025-10-22 22:04:29 +0300

    feat: add Toaster component for notifications in RootLayout

--- kelal/app/layout.tsx
+++ kelal/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProviderWrapper } from "./components/ThemeProviderWrapper";
+import { Toaster } from "@/components/ui/sonner"
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProviderWrapper>{children}</ThemeProviderWrapper>
+        <Toaster position="top-right" richColors />
       </body>
     </html>
   );
