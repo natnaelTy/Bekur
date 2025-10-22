64f4176fdad88e50251ed772567164e219d4bc66
Natnael taye <natitaye316@gmail.com>
2025-10-22 22:05:57 +0300

    feat: implement authentication setup with better-auth and Prisma

---
+++ kelal/lib/auth.ts
@@ -0,0 +1,29 @@
+// lib/auth.ts
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "mysql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  user: {
+    modelName: "User",
+    fields: {
+      name: "fullName",
+      phoneNumber: "phoneNumber",
+    },
+  },
+
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+  },
+});
