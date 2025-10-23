21eb37e6bb81edb69caf949655dd16415e077200
Natnael taye <natitaye316@gmail.com>
2025-10-23 23:06:58 +0300

    feat: add handleGoogleSignIn function for Google authentication

--- kelal/lib/auth-client.ts
+++ kelal/lib/auth-client.ts
@@ -6,4 +6,11 @@ export const authClient = createAuthClient({
   baseURL: "http://localhost:3000/api/auth",
 });
 
+export const handleGoogleSignIn = async () => {
+  const data = await authClient.signIn.social({
+    provider: "google"
+  });
+  return data;
+};
+
 export const { signUp, signIn, signOut, useSession } = authClient;
