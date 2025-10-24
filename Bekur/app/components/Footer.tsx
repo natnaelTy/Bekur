d4cfba26346e4c354adc47ddd2aa95de983cc476
Natnael taye <natitaye316@gmail.com>
2025-10-24 22:08:05 +0300

    feat: update Footer layout and improve copyright text styling

--- Bekur/app/components/Footer.tsx
+++ Bekur/app/components/Footer.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
-    <footer className='bg-gray-50 dark:bg-gray-950 border-t-1 border-gray-200 dark:border-gray-900 px-20 py-10 flex flex-col md:flex-row items-center justify-between gap-4'>
-      <p>Copyright &copy; {new Date().getFullYear()} Gerami. All right reserved.</p>
-      <p className='text-gray-800 dark:text-white'>Powered by <span className='font-medium text-gray-900 dark:text-white'>Mistral</span> and <span className='font-medium text-gray-900 dark:text-white'>Langflow</span></p>
+    <footer className="bg-gray-50 dark:bg-gray-950 border-t-1 border-gray-200 dark:border-gray-900 px-4 py-6 md:px-20 md:py-10 flex flex-col md:flex-row items-start md:items-center justify-between gap-4">
+      <p className="text-gray-800 dark:text-white">
+        Powered by{" "}
+        <span className="font-medium text-gray-900 dark:text-white">
+          Mistral
+        </span>{" "}
+        and{" "}
+        <span className="font-medium text-gray-900 dark:text-white">
+          Langflow
+        </span>
+      </p>
+      <p>
+        Copyright &copy; {new Date().getFullYear()} Bekur. All right reserved.
+      </p>
     </footer>
-  )
+  );
 }
