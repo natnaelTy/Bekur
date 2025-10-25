ecf79a1300fb996b265a5fb58afd7fb75407602f
Natnael taye <natitaye316@gmail.com>
2025-10-25 22:46:13 +0300

    feat: add ApplicationDetails component to display application information

---
+++ Bekur/app/(account)/dashboard/components/ApplicationDetails.tsx
@@ -0,0 +1,35 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function ApplicationDetails() {
+  const appData = {
+    fullName: "Natnael Taye",
+    program: "Computer Science",
+    university: "Tokyo University",
+    dateApplied: "Oct 5, 2025",
+  };
+
+  return (
+    <Card className="bg-white dark:bg-gray-950">
+      <CardHeader>
+        <CardTitle>Application Details</CardTitle>
+      </CardHeader>
+      <CardContent className="space-y-2 text-sm md:text-base">
+          <p className="text-sm md:text-base font-semibold">
+          Full name:{" "}
+          <span className="text-gray-500 font-normal">
+            {appData.fullName}
+          </span>
+        </p>
+        <p className="text-sm md:text-base font-semibold">
+          Program: <span className="text-gray-500 font-normal">{appData.program}</span>
+        </p>
+        <p className="text-sm md:text-base font-semibold">
+          University: <span className="text-gray-500 font-normal">{appData.university}</span>
+        </p>
+        <p className="text-sm md:text-base font-semibold">
+          Date Applied: <span className="text-gray-500 font-normal">{appData.dateApplied}</span>
+        </p>
+      </CardContent>
+    </Card>
+  );
+}
