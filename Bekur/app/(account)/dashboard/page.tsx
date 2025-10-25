17a940f8480de7da79b0aaca4c8abe54532834a3
Natnael taye <natitaye316@gmail.com>
2025-10-25 22:46:06 +0300

    feat: add DashboardPage component with progress and application details

---
+++ Bekur/app/(account)/dashboard/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import ProgressCard from "./components/ProgressCard";
+import ApplicationDetails from "./components/ApplicationDetails";
+import Scholarships from "./components/Scholarships";
+import InterviewPrepCard from "./components/InterviewPrepCard";
+import PaymentDetails from "./components/PaymentDetails";
+import { useState } from "react";
+
+export default function DashboardPage() {
+  const [hasApplied] = useState(true);
+  const [status] = useState("Accepted");
+  const [progress] = useState(20);
+
+  return (
+    <div className="w-full py-30 lg:py-20 px-1 md:px-4">
+      <h1 className="text-2xl md:text-3xl font-bold mb-6">Selam, Nati Taye 👋</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+        <ProgressCard status={status} progress={progress} />
+        <ApplicationDetails />
+        <PaymentDetails />
+        <InterviewPrepCard />
+        <Scholarships hasApplied={hasApplied} />
+      </div>
+    </div>
+  );
+}
