61a27ec926314cf73efe57cb91c01def7f5e10fa
Natnael taye <natitaye316@gmail.com>
2025-10-27 21:44:32 +0300

    feat: enhance DashboardPage with user session handling and improved layout

--- Bekur/app/(account)/dashboard/page.tsx
+++ Bekur/app/(account)/dashboard/page.tsx
@@ -6,15 +6,22 @@ import Scholarships from "./components/Scholarships";
 import InterviewPrepCard from "./components/InterviewPrepCard";
 import PaymentDetails from "./components/PaymentDetails";
 import { useState } from "react";
+import { useSession } from "@/lib/auth-client";
 
 export default function DashboardPage() {
+  const {data: session} = useSession();
+  const user = session?.user;
+  
   const [hasApplied] = useState(true);
   const [status] = useState("Accepted");
   const [progress] = useState(20);
 
   return (
-    <div className="w-full py-24 px-2 md:px-10 bg-gray-50 dark:bg-gray-950">
-      <h1 className="text-2xl md:text-3xl font-bold mb-6">Selam, Nati Taye 👋 </h1>
+    <div className="w-full py-24 px-3 md:px-10 bg-gray-50 dark:bg-gray-950">
+      <div className="space-y-2 mb-6">
+      <h1 className="text-3xl md:text-4xl font-bold">Selam, {user?.name} 👋🏽</h1>
+      <p className="text-gray-600 dark:text-gray-400 text-sm md:text-base max-w-xl">Welcome to your dashboard. Here you can find all the information related to your application.</p>
+      </div>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
         <ProgressCard status={status} progress={progress} />
         <ApplicationDetails />
