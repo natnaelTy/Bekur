36dba2f5b96c7ccfb3334e73dac7864f057fcd0e
Natnael taye <natitaye316@gmail.com>
2025-10-29 23:39:19 +0300

    feat: refactor ThemeSwitcher component to use dropdown menu for theme selection

--- Bekur/app/components/ThemeToggle.tsx
+++ Bekur/app/components/ThemeToggle.tsx
@@ -1,16 +1,59 @@
 "use client";
 
-import { ThemeSwitcher } from "@/components/kibo-ui/theme-switcher";
-import { useTheme } from "next-themes";
+import React from "react";
+import { LaptopMinimalIcon, MoonIcon, SunIcon } from "lucide-react";
 
-const Example = () => {
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export default function ThemeSwitcher() {
+  const [open, setOpen] = useState(false);
   const { theme, setTheme } = useTheme();
+  const [value, setValue] = useState("system");
+
+  useEffect(() => {
+    if (theme === "light" || theme === "dark" || theme === "system") {
+      setValue(theme);
+    }
+  }, [theme]);
+
+  const select = (val: string) => {
+    setValue(val);
+    setTheme(val);
+    setOpen(false);
+  };
 
   return (
-    <ThemeSwitcher
-      onChange={(value) => setTheme(value)}
-    />
+    <DropdownMenu open={open} onOpenChange={setOpen}>
+      <DropdownMenuTrigger asChild>
+        <Button className="bg-transparent hover:bg-gray-100 dark:hover:bg-gray-900 text-black dark:text-white">
+          {value === "light" ? (
+            <SunIcon />
+          ) : value === "dark" ? (
+            <MoonIcon />
+          ) : (
+            <LaptopMinimalIcon />
+          )}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="dark:bg-gray-950">
+        <DropdownMenuItem onClick={() => select("light")} className=" hover:dark:bg-gray-900">
+          <SunIcon className="mr-2" /> Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => select("system")} className=" hover:dark:bg-gray-900">
+          <LaptopMinimalIcon className="mr-2" /> System
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => select("dark")} className=" hover:dark:bg-gray-900">
+          <MoonIcon className="mr-2" /> Dark
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
-};
-
-export default Example;
+}
