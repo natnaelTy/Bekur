60daa0061a5fedb55cf547bf57a2dc8fbca381ca
Natnael taye <natitaye316@gmail.com>
2025-11-01 22:45:43 +0300

    fix: update default button styling for improved accessibility and consistency

--- Bekur/components/ui/button.tsx
+++ Bekur/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-black/90 dark:bg-white/95 rounded-full dark:text-black text-white hover:bg-black/80 dark:hover:bg-white/80 focus-visible:ring-black/20 dark:focus-visible:ring-white/40",
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
@@ -22,8 +22,8 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        sm: "h-8 rounded-full gap-1.5 px-3 has-[>svg]:px-2.5",
+        lg: "h-10 rounded-full px-6 has-[>svg]:px-4",
         icon: "size-9",
         "icon-sm": "size-8",
         "icon-lg": "size-10",
