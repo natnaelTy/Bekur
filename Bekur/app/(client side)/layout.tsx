4011bc6d2131f4172679526dd6507bb3d59c2fba
Natnael taye <natitaye316@gmail.com>
2025-11-17 22:29:45 +0300

    fix: remove ChatbotPage component from ClientLayout for cleaner structure

--- Bekur/app/(client side)/layout.tsx
+++ Bekur/app/(client side)/layout.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import { ThemeProvider } from "next-themes";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import ChatbotPage from "../components/ChatBot";
+
 
 function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
       <main>{children}</main>
-        <ChatbotPage />
       <Footer />
     </>
   );
