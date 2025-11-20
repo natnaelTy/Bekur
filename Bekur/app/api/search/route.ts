2f84832f8728f3fe5378118a319250be1c0b49f5
Natnael taye <natitaye316@gmail.com>
2025-11-20 20:27:36 +0300

    refactor: update search opportunities API to include additional fields and validation

--- Bekur/app/api/search/route.ts
+++ Bekur/app/api/search/route.ts
@@ -1,23 +1,54 @@
 import { findOpportunities } from "@/lib/langflow";
 import { NextResponse } from "next/server";
-import type { NextRequest } from 'next/server';
+import type { NextRequest } from "next/server";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 
 export default async function searchOpportunities(req: NextRequest) {
-  const { fullName, country, fieldOfStudy, gpa, goals } = await req.json();
+  const {
+    fullName,
+    phoneNumber,
+    purpose,
+    passport,
+    country,
+    program,
+    dateOfBirth,
+  } = await req.json();
 
   try {
-    const result = await findOpportunities({
-      fullName: fullName,
-      country: country,
-      fieldOfStudy: fieldOfStudy,
-      gpa: gpa,
-      goals: goals,
-    });
+    if (
+      !fullName ||
+      !country ||
+      !program ||
+      !purpose ||
+      !phoneNumber ||
+      !passport ||
+      !dateOfBirth
+    ) {
+      return NextResponse.json({ message: "All fields are required" });
+    }
 
-    console.log("Search Opportunities Result:", result);
+    const applications = await prisma.userApplication.findMany();
+    if (applications) {
+      return NextResponse.json({ message: "you can't apply again" });
+    } else {
+      const newApplication = await prisma.userApplication.create({
+        data: {
+          fullName,
+          phoneNumber,
+          purpose,
+          passport,
+          country,
+          program,
+          dateOfBirth,
+        },
+      });
+    }
 
-    return NextResponse.json(result);
+    console.log("Search Opportunities Result:", applications);
+
+    return NextResponse.json(applications);
   } catch (error) {
     console.error("Search Opportunities Error:", error);
     return NextResponse.json({ matches: [] });
