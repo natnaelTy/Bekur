deb222a81d48aad72b2b6060a85f0698e5b044c9
natnaelTy <natitaye316@gmail.com>
2025-12-07 21:58:59 +0300

    feat: add Prisma configuration for database connection

---
+++ Bekur/prisma.config.ts
@@ -0,0 +1,10 @@
+
+import { defineConfig, env } from 'prisma/config';
+import 'dotenv/config'; 
+
+export default defineConfig({
+  datasource: {
+    url: env('DATABASE_URL'), 
+  },
+  schema: 'prisma/schema.prisma', 
+});
