e99f1a4ae29f231fdb097958c012c06e33cfeacd
natnaelTy <natitaye316@gmail.com>
2025-12-14 21:18:20 +0300

    refactor: update prisma import path for consistency and clarity

--- Bekur/lib/auth.ts
+++ Bekur/lib/auth.ts
@@ -1,7 +1,7 @@
 // lib/auth.ts
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { prisma } from "@/prisma/prismaClient";
+import { prisma } from "@/lib/prisma";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
