03c143632897752e1e002ed74d61b88a41a44d64
natnaelTy <natitaye316@gmail.com>
2025-12-18 22:13:20 +0300

    fix: update LANGFLOW_BASE_URL to use service name and enhance logging

--- Bekur/app/api/chat/route.ts
+++ Bekur/app/api/chat/route.ts
@@ -1,9 +1,10 @@
 import { LangflowClient } from "@datastax/langflow-client";
 
-const LANGFLOW_BASE_URL = "http://localhost:7860";
+const LANGFLOW_BASE_URL = "http://langflow:7860";
 
 const FLOW_ID =
   process.env.LANGFLOW_FLOW_ID || "";
+  
 const API_KEY = process.env.LANGFLOW_API_KEY;
 
 const client = new LangflowClient({
@@ -14,17 +15,21 @@ const client = new LangflowClient({
 export async function POST(req: Request) {
   const { message } = await req.json();
 
+  console.log("Received message:", message);
+
   try {
     const flow = client.flow(FLOW_ID);
 
     const response = await flow.run(message);
 
     const finalResponse = response.chatOutputText();
-
+    console.log("Langflow response:", finalResponse);
     return new Response(JSON.stringify({ response: finalResponse }), {
       status: 200,
       headers: { "Content-Type": "application/json" },
     });
+
+
   } catch (error: any) {
     console.error("Langflow Client Error:", error);
     return new Response(
