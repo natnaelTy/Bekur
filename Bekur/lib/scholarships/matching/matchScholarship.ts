fbe22bdb02e923750e8413b66323d189ef717a6a
natnaelTy <natitaye316@gmail.com>
2025-12-23 23:14:54 +0300

    feat: update matchScholarship function to use eligible countries for matching criteria

--- Bekur/lib/scholarships/matching/matchScholarship.ts
+++ Bekur/lib/scholarships/matching/matchScholarship.ts
@@ -1,19 +1,18 @@
 import { Scholarship } from "../../types/scholarship";
 import { StudentProfile } from "../../types/student";
 
-
 export function matchScholarship(
   scholarship: Scholarship,
   student: StudentProfile
 ): boolean {
+
   if (
-    scholarship.eligible_countries.length > 0 &&
-    !scholarship.eligible_countries.includes(student.country_applying_to)
+    scholarship.country &&
+    scholarship.country !== student.country_applying_to
   ) {
     return false;
   }
 
-  // Level
   if (
     scholarship.level.length > 0 &&
     !scholarship.level.includes(student.level)
@@ -21,7 +20,6 @@ export function matchScholarship(
     return false;
   }
 
-  // Field
   if (
     scholarship.fields_of_study.length > 0 &&
     !scholarship.fields_of_study.includes(student.field)
