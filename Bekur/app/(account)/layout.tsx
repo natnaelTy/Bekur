46c4646d9ca1184c88927c0fef53e0028caf6d65
natnaelTy <natitaye316@gmail.com>
2026-01-12 21:13:51 +0300

    feat: refactor ProtectedLayout to include session validation and loading state

--- Bekur/app/(account)/layout.tsx
+++ Bekur/app/(account)/layout.tsx
@@ -1,13 +1,32 @@
-import React from "react";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+"use client";
 
-function AccountLayout({ children }: { children: React.ReactNode }) {
+import { redirect } from "next/navigation";
+import { authClient } from "@/lib/auth-client";
+import Navbar from "../components/Navbar";
+import Loader from "@/components/kokonutui/loader";
+
+export default function ProtectedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { data: session, isPending } = authClient.useSession();
+  const isAuthenticated = !!session;
+
+  if (isPending || !session) {
+    return (
+      <Loader size="md" className="flex items-center justify-center h-screen" />
+    );
+  }
+
+  if (!isAuthenticated) {
+    redirect("/signin");
+  }
+
   return (
-    <div>
+    <>
       <Navbar />
       {children}
-    </div>
+    </>
   );
 }
-export default AccountLayout;
