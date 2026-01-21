bfce6db186cf868d13cddb66f7ffe2796b9f0b2a
natnaelTy <natitaye316@gmail.com>
2026-01-21 23:13:19 +0300

    refactor: implement logout functionality with error handling and redirect

--- Bekur/components/nav-user.tsx
+++ Bekur/components/nav-user.tsx
@@ -7,6 +7,9 @@ import {
   IconNotification,
   IconUserCircle,
 } from "@tabler/icons-react"
+import { signOut } from "@/lib/auth-client"
+import React from "react"
+import { redirect } from "next/navigation"
 
 import {
   Avatar,
@@ -39,6 +42,16 @@ export function NavUser({
   }
 }) {
   const { isMobile } = useSidebar()
+
+  const handleLogout = async (event: React.SyntheticEvent) => {
+    event.preventDefault()
+    try {
+      await signOut()
+      redirect("/admin/login")
+    } catch (error) {
+      console.error("Failed to sign out", error)
+    }
+  }
 
   return (
     <SidebarMenu>
@@ -98,7 +111,7 @@ export function NavUser({
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>
+            <DropdownMenuItem onClick={handleLogout}>
               <IconLogout />
               Log out
             </DropdownMenuItem>
